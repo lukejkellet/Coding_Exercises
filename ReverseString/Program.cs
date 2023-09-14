12ae0db6c12e942bde01d4b1c219f4f4fda96f4c
Luke Kellet <lukejameskellet@hotmail.com>
2023-09-14 15:20:49 +0100

    Finished code.

---
+++ ReverseString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Hello world";
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                Console.Write(input[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
