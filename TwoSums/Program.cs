253cd736cc617cd449c8f8def339d4972756f51b
Luke Kellet <lukejameskellet@hotmail.com>
2023-09-14 15:55:14 +0100

    Finished code.

--- TwoSums/Program.cs
+++ TwoSums/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TwoSums
 {
@@ -10,6 +7,30 @@ namespace TwoSums
     {
         static void Main(string[] args)
         {
+            Tuple<int, int> result = FindTwoSum(new List<int>() { 3, 1, 5, 7, 5, 9 }, 10);
+            if (result != null)
+            {
+                Console.WriteLine("[" + result.Item1 + ", " + result.Item2 + "]");
+            }
+            else { Console.WriteLine("No pairs."); }
+            Console.ReadLine();
+        }
+
+        public static Tuple<int, int> FindTwoSum(List<int> list, int targetSum)
+        {
+            for (int i = 0; i < list.Count; i++) // Loop through the list
+            {
+                // Starts j at index 1 to avoid checking the same index twice
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    //Checks if the sum of the two numbers is equal to the target sum
+                    if (list[i] + list[j] == targetSum) 
+                    {
+                        return new Tuple<int, int>(i, j); //Returns the index values of the two numbers
+                    }
+                }
+            }
+            return null;
         }
     }
 }
